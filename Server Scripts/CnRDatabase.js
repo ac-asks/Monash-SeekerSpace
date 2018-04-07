76a165b3da0d35792c05473a0ae6ac5ba2019556
Trinityy2 <tomohito95@hotmail.com>
2018-04-07 17:19:42 +1000

    Moving Scripts around...

--- Server Scripts/CnRDatabase.js
+++ Server Scripts/CnRDatabase.js
@@ -17,12 +17,33 @@ con.connect(function(err) {
   {
     if (err) throw err;
     console.log("Database created");
-  });*/
+  });
 
   //Creates a Table
   var sql = "CREATE TABLE robbers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), longitude DOUBLE(23,4), latitude DOUBLE(23,4))";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    console.log("cops deleted");
   });
+
+  con.query("DROP TABLE cops", function(err, result){
+    if (err) throw err;
+    console.log("cops deleted");
+  });
+
+  con.query("DROP TABLE robbers", function(err, result){
+    if (err) throw err;
+    console.log("robbers created");
+  });
+  */
+
+//Where for identity:
+//0 = No identity
+//1 = cop
+//2 = Robber
+  con.query("CREATE TABLE players (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), longitude DOUBLE(23, 4), latitude DOUBLE (23,4), identity INT(1))", function (err, result) {
+    if (err) throw err;
+    console.log("players created");
+  });
+
 });
