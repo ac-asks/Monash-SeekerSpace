4bd8dfadb425269a7f970bcb3bded5299d6448b4
Peter <peterbui1995@gmail.com>
2018-04-07 16:54:32 +1000

    reformatted

--- Testing lat+long/Latitude_n_Longitude.js
+++ Testing lat+long/Latitude_n_Longitude.js
@@ -1,24 +1,23 @@
 // attempts to store lat and long into a format that can be used
 var y = document.getElementById("lat+lon");
-// Stores lat and lon as global variables so that we can manipulate them
-
+/* Stores lat and lon as global variables so that we can manipulate them
+*/
 var lat = 0;
 var lon = 0;
 
 // Function retrives lat + lon and calls printLat
 function positionFunc() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(retreiveLatnLon);
+        navigator.geolocation.getCurrentPosition(success);
     } else {
         y.innerHTML = "you suck";
     }
-    printLat(lat, lon);
 }
-
-// Retrieves Latitude and Longitude and returns them by hanging the global variable
-function retreiveLatnLon(position) {
-    lat = position.coords.latitude
-    lon = position.coords.longitude
+// changes global variable lat and lon
+function success(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    printLat(lat, lon);
 }
 
 // Prints lat + lon
