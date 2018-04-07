31f5c8afcf3d05eebe45f722c93b40a616858977
Peter <peterbui1995@gmail.com>
2018-04-07 18:24:37 +1000

    gave up on node.js

--- Testing lat+long/node.js test/server.js
+++ Testing lat+long/node.js test/server.js
@@ -7,5 +7,5 @@ function onRequest(request, response) {
     response.end();
 }
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(8080);
 console.log("Server is now running...");
