622c1bf6b22902ed37888ccc8c5b2f927812bd27
Trinityy2 <tomohito95@hotmail.com>
2018-04-08 12:16:02 +1000

    oops

--- startbootstrap-freelancer-gh-pages/ServerScript.js
+++ startbootstrap-freelancer-gh-pages/ServerScript.js
@@ -11,7 +11,7 @@ var players = [];
 //setup an express app
 var app = express();
 
-  app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded(
   {extended: true}
